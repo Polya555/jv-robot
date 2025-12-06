62e3d9ac690b9c9ef26709249a529a8f84f11bec
Polina <klimashevscapo@gmail.com>
2025-12-06 21:23:15 +0200

    did all required tasks

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,61 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction needX = null;
+        Direction needY = null;
+        if (toX > robot.getX()) {
+            needX = Direction.RIGHT;
+        } else if (toX < robot.getX()) {
+            needX = Direction.LEFT;
+        }
+        if (toY > robot.getY()) {
+            needY = Direction.UP;
+        } else if (toY < robot.getY()) {
+            needY = Direction.DOWN;
+        }
+
+        if (needX != null) {
+            int countX = Math.abs(toX - robot.getX());
+            turnTo(robot, needX);
+            for (int i = 0; i < countX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (needY != null) {
+            int countY = Math.abs(toY - robot.getY());
+            turnTo(robot, needY);
+            for (int i = 0; i < countY; i++) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    private void turnTo(Robot robot, Direction needed) {
+        Direction current = robot.getDirection();
+        while (current != needed) {
+            current = robot.getDirection();
+            if (current == needed) {
+                return;
+            }
+        }
+        if (stepForward(current, needed)) {
+            robot.turnRight();
+        } else {
+            robot.turnLeft();
+        }
+    }
+
+    private boolean stepForward(Direction current, Direction needed) {
+        if (current == Direction.UP) {
+            return needed == Direction.RIGHT;
+        } else if (current == Direction.RIGHT) {
+            return needed == Direction.DOWN;
+        } else if (current == Direction.DOWN) {
+            return needed == Direction.LEFT;
+        } else if (current == Direction.LEFT) {
+            return needed == Direction.UP;
+        }
+        return false;
     }
 }
